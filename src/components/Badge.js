b3a8a78be66b257acbfc2880ea8b2edadbd5e910
Fermin Martin <fermartorr01@gmail.com>
2020-11-17 00:13:58 +0100

    CREACIÓN Y DISEÑO DE COMONENTES: Nuestro primer componente
    -Los componentes son clases y van en achivos nuevos
    -Creo la clase extendiendo de React.Component
    -Hacemos el método render con código ej jsx
    -Exportamos badge.js
    -En index.js importamos Badge
    -En ReactDOM.render coloco el <Badge/> dentro del container

---
+++ src/components/Badge.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import confLogo from '../images/confLogo.svg';
+
+class Badge extends React.Component {
+  render() {
+    return  <div>
+      <div>
+        <img src={confLogo} alt="Logo de la conferencia"/>
+      </div>
+      <div>
+        <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+        <h1>Richard <br/> Kauffman</h1>
+      </div>
+      <div>
+        <p>Frontend Engineer</p>
+        <p>@asparragus</p>
+      </div>
+      <div>
+        #platziconf
+      </div>
+    </div>;
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello Platzi! Yeah';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const jsx = <h1>Hello Platzi!</h1>;
-// const element = React.createElement(
-//   'a',
-//   { href: 'https://platzi.com' },
-//   'Ir');
-// const name = 'Angela';
-// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
-// const jsx = <h1>Hola, soy {name}</h1>
-
-const jsx = (<div>
-  <h1>Hola, soy Fermin</h1>
-  <p>Soy ingeniero frontend</p>
-</div>); //jsx
-
-const element = React.createElement(
-  'div',
-  {},
-  React.createElement('h1', {}, 'Hola, soy Fermin'),
-  React.createElement('p', {}, 'Soy ingeniera frontend')
-); //React.createElement
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__)
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
