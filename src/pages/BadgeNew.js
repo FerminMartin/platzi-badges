d16ef30cca24ae02016e35e38d68b46b46675381
Fermin Martin <fermartorr01@gmail.com>
2020-11-17 21:02:13 +0100

    CREACIÓN Y DISEÑO DE COMPONENTES: Primera Página
    HEADER ó HERO
    -Esta sección la creo en la misma página BadgeNew.js
    -Añado un div y dentro la img
    -importo la img y le aplico la clase de bootstrap "img-fluid"

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import header from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
 class BadgeNew extends React.Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo"/>
+        </div>
       </div>
     );
   }
