330c2b00f736c2feefebb2d056b891aafb2caceb
Fermin Martin <fermartorr01@gmail.com>
2020-11-17 20:50:08 +0100

    CREACIÓN Y DISEÑO DE COMPONENTES: Primera Página
    -aplico estilos a la parte de brand y logo (Navbar.css)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ class Navbar extends React.Component {
     return (
       <div className="Navbar">
         <div className="container-fluid">
-          <a href="/">
-            <img src={confLogo} alt="Logo" />
+          <a className="Navbar__brand" href="/">
+            <img className="Navbar__brand-logo" src={confLogo} alt="Logo" />
             <span className="font-weight-light">Platzi</span>
             <span className="font-weight-bold">Conf</span>
           </a>
