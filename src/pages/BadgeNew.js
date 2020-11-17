271cabba3862202f8368227b77aba00f9080b1dc
Fermin Martin <fermartorr01@gmail.com>
2020-11-17 21:43:20 +0100

    CREACIÓN Y DISEÑO DE COMPONENTES: Primera Página
    BADGE:
    -Creo un contenedor
    -Dentro una fila que lleva entro una columna
    -Dentro de la columna llamo al componente ó clase Badge
    -Dentro de <Badge/> llamo a las PROPS
    -NOTA: Al componente se le conoce por la Mayúscula entre llaves seguido de sus props

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
 import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
 class BadgeNew extends React.Component {
   render() {
     return (
@@ -10,6 +11,20 @@ class BadgeNew extends React.Component {
         <Navbar />
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo"/>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge 
+                firstName="Fermin" 
+                lastName="Martin" 
+                twitter="fermartorr" 
+                jobTitle="Frontend Engineer" 
+                avatarUrl="https://s.gravatar.com/avatar/930f34bb64181c4cfc8a72d264fc90be?s=80"
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
