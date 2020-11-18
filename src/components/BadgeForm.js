ef4317459de43dff55b334b4c087f836f673ca29
Fermin Martin <fermartorr01@gmail.com>
2020-11-18 12:46:09 +0100

    ENLAZANDO EVENTOS: Eventos
    -Para leer la info del texto crear EVENTOS
    -En Input va el evento onChange que llama a método handleChange
    -Declaro el método onChange handleChange poniendo un name y un value
    -Name muestra la propiedad name del input y value su valor
    -Declaro el Evento onClick y el método handleClick
    -Declaro el Evento onSubmit y el handleSubmit
    -HandleSubmit tiene un preventDefault() para que no envíe el formulario

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+Eventol  handleChange = (e) => {
+    console.log({
+      name: e.target.name,
+      value: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    console.log('Button was clicked');
+  };
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form was submitted');
+  };
+
   render() {
     return(
       <div>
         <h1>New Attendat</h1>
 
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>First Name</label>
-            <input className="form-control" type="text" name="firstName"/>
+            <input onChange={this.handleChange} className="form-control" type="text" name="firstName"/>
           </div>
-          <button className="btn btn-primary">Save</button>
+          <button onClick={this.handleClick} className="btn btn-primary">Save</button>
         </form>
       </div>
     );
