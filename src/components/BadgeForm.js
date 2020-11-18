394b4d84dd126c5e841f5911fe4916c241766da5
Fermin Martin <fermartorr01@gmail.com>
2020-11-18 11:23:17 +0100

    ENLAZANDO EVENTOS: Formulario
    -Form > div con className="form-group" de Bootstrap.
    -"form-group" agrega los mismos estilos a un grupo de etiquetas. Es de Bootstrap
    -Agrego <label> e <input> (className="form-control" de Bootstrap")

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -3,8 +3,17 @@ import React from 'react';
 class BadgeForm extends React.Component {
   render() {
     return(
-      <p>BadgeForm</p>
-    )
+      <div>
+        <h1>New Attendat</h1>
+
+        <form></form>
+          <div className="form-group">
+            <label>First Name</label>
+            <input className="form-control" type="text" name="firstName"/>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
