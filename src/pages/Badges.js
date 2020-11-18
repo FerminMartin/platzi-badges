2ea79ee5d87b2e106ea3286524ce83d6f44233ee
Fermin Martin <fermartorr01@gmail.com>
2020-11-19 00:13:01 +0100

    LISTAS DE COMPONENTES:
    -Create Navbar and hero section (with styles)

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import './styles/Badges.css';
+import confLogo from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
+
 class Badges extends React.Component {
   render() {
-    return 'Badges'
+    return (
+      <div>
+        <Navbar />
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img classNAme="Badges_conf-logo" src={confLogo} alt="Conf Logo"/>
+            </div>
+          </div>
+        </div>
+
+        
+
+      </div>
+    );
   }
 }
 
