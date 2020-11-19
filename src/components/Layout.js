3fd1d82689482792cc7f4d8d2cb4e37caf1f6020
Fermin Martin <fermartorr01@gmail.com>
2020-11-19 21:38:54 +0100

    REACT ROUTER: Improving User Interface with a Layout
    -Use <React.Fragment> instead of empty <div> and it wont appear in navBrowser
    
    Note: It´s a tip for a clean code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,10 @@ import Navbar from './Navbar';
 function Layout (props) {
     // const children = props.children;
     return (
-      <div>
+      <React.Fragment>
         <Navbar />
         {props.children}
-      </div>
+      </React.Fragment>
       );
 }
 
