d30ae36c82362c8431ce6111b91cfa5fb7c9346d
Fermin Martin <fermartorr01@gmail.com>
2020-11-19 21:28:42 +0100

    REACT ROUTER: Improving User Interface with a Layout
    -Remove <Navbar /> and its imports from pages and move to Layout.js
    -Import Navbar into Layout.js
    -Function Layout(props) return (in jsx) <div><Navbar />{props.children}
    
    Note: Layout creates Navbar above its children props.children (2 pages)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 function Layout (props) {
     // const children = props.children;
-    return props.children;
+    return (
+      <div>
+        <Navbar />
+        {props.children}
+      </div>
+      );
 }
 
 export default Layout; 
