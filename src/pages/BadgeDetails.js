bd71edecca57775b284169bfb192ef8d8f0ee730
Fermin Martin <fermartorr01@gmail.com>
2020-11-23 23:43:36 +0100

    DETAILS OF A BADGE (Improving UI)
    -Set structure for the header

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import './styles/BadgeDetails.css';
+import confLogo from '../images/platziconf-logo.svg';
+
 class BadgeDetails extends React.Component {
   render() {
-    return 'BadgeDetails'
+    return (
+      <div>
+        <div className="BadgeDetails__hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <img src={confLogo} alt="Logo de la Conferencia"/>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
   }
 }
 
