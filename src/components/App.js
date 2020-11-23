ee9a5ab12b1d61805d9fc9eabd886238459ed202
Fermin Martin <fermartorr01@gmail.com>
2020-11-23 18:42:17 +0100

    UPDATE DATA (PUT)
    -Create route in App.js
    -This new route has a badgeId on its path:
    -<Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
    -Create BadgeEdit.js and BadgeEdit.css (duplicating BadgeNew)

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from './Layout';
 import BadgeHome from '../pages/BadgeHome';
 import BadgeNew from '../pages/BadgeNew';
+import BadgeEdit from '../pages/BadgeEdit';
 import Badges from '../pages/Badges';
 import NotFound from '../pages/NotFound';
 
@@ -13,8 +14,9 @@ function App() {
       <Layout>
         <Switch>
           <Route exact path="/" component={BadgeHome} />
-          <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges" component={Badges} />
+          <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
       </Layout>
