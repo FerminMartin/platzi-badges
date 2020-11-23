9e07f1319ba156b20031de98dc708bdd4916d1ac
Fermin Martin <fermartorr01@gmail.com>
2020-11-23 16:54:39 +0100

    IMPROVING UE DURING REQUEST resolving the challenge
    -Set the img on the new avatar importing Gravatar from components
    -Change img fo <Gravatar> with props email

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import Gravatar from '../components/Gravatar';
 
 import './styles/BadgesList.css';
 class BadgesList extends React.Component {
@@ -20,7 +21,11 @@ class BadgesList extends React.Component {
           {this.props.badges.map((badge) =>{
             return (
               <li key={badge.id} className="BadgesListItem">
-                <img src={badge.avatarUrl} alt="" className="BadgesListItem__avatar"/>
+                <Gravatar 
+                  className="BadgesListItem__avatar"
+                  email={badge.email} 
+                  alt="Avatar"
+                />
                 <div>
                   <div><strong>{badge.firstName} {badge.lastName}</strong></div>
                   <div className="Twitter__name">
